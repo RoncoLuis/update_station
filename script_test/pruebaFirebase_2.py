b3b49ce1e90de816bc1b4001d9ad6497542b2f81
Luis Ronquillo <w580ix@gmail.com>
2019-09-16 13:44:21 -0500

    Cambios en pruebaFirebase2.py. Ya inserta el dato con la fecha y hora en firebase. este commit es antes de pasar todo lo hecho en script, al principal de barcodeReader2

--- script_test/pruebaFirebase_2.py
+++ script_test/pruebaFirebase_2.py
@@ -3,21 +3,22 @@ from firebase_admin import credentials
 from firebase_admin import db
 import json
 import time
+from script_main import generar_qr
 
-p_001 ={"id":"p_002","nombre":"universidad"}
-p_001['fecha'] = time.strftime("%d/%m/%y")
-p_001['hora'] = time.strftime("%H:%M:%S")
+json_data = generar_qr.make_json_data('p_001','Delta-Universidad','alimentadora')
+json_data['fecha'] = time.strftime("%d/%m/%y")
+json_data['hora'] = time.strftime("%H:%M:%S")
+generar_qr.make_qr(json_data,'prueba_1')
 
-encoded = json.dumps(p_001)
+encoded = json.dumps(json_data)
 decoded = json.loads(encoded)
 print(decoded)
 #Fetch the service account key JSON file contents
-#credentials = credentials.Certificate('../ficheros_salidas/tst-update-station-firebase-adminsdk-jq8g5-71c9c42d8b.json')
+credentials = credentials.Certificate('../ficheros_salidas/tst-update-station-firebase-adminsdk-jq8g5-71c9c42d8b.json')
 
 #initialize the app with a service account, granting admin privileges
-#firebase_admin.initialize_app(credentials,{
-#    'databaseURL':'https://tst-update-station.firebaseio.com/'
-#})
+firebase_admin.initialize_app(credentials,{'databaseURL':'https://tst-update-station.firebaseio.com/'})
 
-#ref = db.reference('stored_data')
-#user_ref = ref.child('updatestation').child('paradero').push(decoded)
+
+ref = db.reference('stored_data')
+user_ref = ref.child('updatestation').child('paradero').push(decoded)
